85c6644013a24f71de21ad99c5c9f4ba614a1b80
Mark Allison <mark.allison75@gmail.com>
2020-02-25 17:36:02 -0500

    Tuesday

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,17 +1,18 @@
-const bcrypt = require("bcryptjs"); // <<<<<<<<<<<<<<<< npm i bcryptjs
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
 const Users = require("../users/users-model.js");
 
+// for endpoints beginning with /api/auth
 router.post("/register", (req, res) => {
   let user = req.body;
-
-  const hash = bcrypt.hashSync(user.password, 8); // << 1
-
-  user.password = hash; // <<<<<<<<<<<<<<<<<<<<<<<<<<<< 2
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
+      req.session.loggedIn = true;
+
       res.status(201).json(saved);
     })
     .catch(error => {
@@ -26,7 +27,12 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
+        req.session.loggedIn = true;
+        req.session.username = user.username;
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+        });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
       }
@@ -34,6 +40,22 @@ router.post("/login", (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({
+          you: "can check out any time you like, but you can never leave",
+        });
+      } else {
+        res.status(200).json({ you: "logged out successfully" });
+      }
+    });
+  } else {
+    res.status(200).json({ bye: "felicia" });
+  }
 });
 
 module.exports = router;
